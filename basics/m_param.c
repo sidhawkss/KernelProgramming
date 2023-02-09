3d84368a15b3f69cfdde6ae0fc3583355d18576b
SidHawks <65626547+sidhawkss@users.noreply.github.com>
2023-02-09 13:55:39 -0300

    Add: ModuleParam

---
+++ basics/m_param.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+// to use the params you need to do:
+// sudo insmod driver.ko times=10 word=test
+//
+
+static int times;
+static char *word;
+module_param(times, int, 0);
+module_param(word, charp, 0);
+
+
+static int __init driver_init(void){
+	printk("Repeating [%d] times\n", times);
+	while(times){
+		printk("WORD= %s\n", word);
+		times--;
+	}	
+
+	return 0;
+}
+
+static void __exit driver_exit(void){
+	pr_info("Exiting...");
+}
+
+
+module_init(driver_init);
+module_exit(driver_exit);
